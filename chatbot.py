7a5c79a01d3282cd0c14541decaeb5e4b382a892
Abinaya Natarajan <haiabiraj@gmail.com>
2024-09-21 10:28:17 -0400

    Create initial Streamlit UI with PDF file uploader

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+#Streamlit for UI Interface
+import streamlit as st
+
+#Upload PDF files
+st.header("My First Chatbot")
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
