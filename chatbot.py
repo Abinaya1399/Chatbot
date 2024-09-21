10a24b9f43c49773f7f9a69eecb9702422638b9f
Abinaya Natarajan <haiabiraj@gmail.com>
2024-09-21 12:55:28 -0400

    Add user input and similarity search with API key removed

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,10 @@
 import streamlit as st #Streamlit for UI Interface
-from PyPDF2 import PdfReader #To read PDF Files
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader #Reads PDF Files and Extracts Text
+from langchain.text_splitter import RecursiveCharacterTextSplitter #Breaks text into chunks
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+#pip install tiktoken
+#pip install openai
 
 #Upload PDF files
 st.header("My First Chatbot")
@@ -16,7 +20,7 @@ if file is not None:
         text+=page.extract_text()
         # st.write(text)
 
-#Break it into chunks
+    #Break it into chunks
     text_splitter = RecursiveCharacterTextSplitter(
         separators = "\n",
         chunk_size = 1000,
@@ -26,3 +30,23 @@ if file is not None:
     chunks = text_splitter.split_text(text)
     # st.write(chunks)
 
+    #Generating embeddings - Use OpenAI services & the OpenAI secret key generated
+    #OPENAI_API_KEY - generate secret key from 'https://platform.openai.com/api-keys'
+    
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    #Creating Vector Store - FAISS (Facebook AI Semantic Search)
+    vector_store = FAISS.from_texts(chunks, embeddings)
+
+    #Get User Question
+    user_question = st.text_input("Type your question here")
+
+    #Do Similarity Search
+    #1. User question(text format) -> embeddings
+    #2. Find similarity between user question's embeddings and vector store's embeddings
+    #3. Corresponding highly ranked chunks will be returned as match
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+        st.write(match)
+
+    #Output Results
