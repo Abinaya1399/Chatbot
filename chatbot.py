2bf154c569dbfb29e222a82e3c9488745a1b0872
Abinaya Natarajan <haiabiraj@gmail.com>
2024-09-21 13:22:43 -0400

    Add LLM setup and QA chain with ChatOpenAI for generating responses

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,15 @@
+# pip install streamlit pypdf2 langchain faiss-cpu openai tiktoken 
+
 import streamlit as st #Streamlit for UI Interface
 from PyPDF2 import PdfReader #Reads PDF Files and Extracts Text
 from langchain.text_splitter import RecursiveCharacterTextSplitter #Breaks text into chunks
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-#pip install tiktoken
-#pip install openai
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
+
+#OPENAI_API_KEY - generate secret key from 'https://platform.openai.com/api-keys'
+OPENAI_API_KEY=""
 
 #Upload PDF files
 st.header("My First Chatbot")
@@ -31,8 +36,6 @@ if file is not None:
     # st.write(chunks)
 
     #Generating embeddings - Use OpenAI services & the OpenAI secret key generated
-    #OPENAI_API_KEY - generate secret key from 'https://platform.openai.com/api-keys'
-    
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
     #Creating Vector Store - FAISS (Facebook AI Semantic Search)
@@ -47,6 +50,21 @@ if file is not None:
     #3. Corresponding highly ranked chunks will be returned as match
     if user_question:
         match = vector_store.similarity_search(user_question)
-        st.write(match)
+        #st.write(match)
 
-    #Output Results
+        #Define LLM
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature=0, #lower the value - no randomness
+            max_tokens = 1000, #limit of the response, 1000 -> around 750 words
+            model_name="gpt-3.5-turbo"
+        )
+
+        #Output Results
+        #chain -> take the que, get relevant doc, pass it to LLM, generate the o/p
+        #stuff - stuff all the documents and pass it to LLM
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents = match, question = user_question)
+        st.write(response)
+
+#To run and execute -> Run the command in terminal: 'streamlit run chatpot.py'
