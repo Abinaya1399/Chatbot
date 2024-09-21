e4789226ab6bd5518c521fbfac7d2c254cc676b4
Abinaya Natarajan <haiabiraj@gmail.com>
2024-09-21 11:46:52 -0400

    Implement text chunking with RecursiveCharacterTextSplitter

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import streamlit as st #Streamlit for UI Interface
 from PyPDF2 import PdfReader #To read PDF Files
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 #Upload PDF files
 st.header("My First Chatbot")
@@ -13,8 +14,15 @@ if file is not None:
     text=""
     for page in pdf_reader.pages:
         text+=page.extract_text()
-        st.write(text)
+        # st.write(text)
 
 #Break it into chunks
-
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators = "\n",
+        chunk_size = 1000,
+        chunk_overlap = 150,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
 
