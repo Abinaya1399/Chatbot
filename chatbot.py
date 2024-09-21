7df1d05f8c5597911d3f0248988b7cb066eb9753
Abinaya Natarajan <haiabiraj@gmail.com>
2024-09-21 11:32:16 -0400

    Add PDF text extraction functionality with PyPDF2

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,20 @@
-#Streamlit for UI Interface
-import streamlit as st
+import streamlit as st #Streamlit for UI Interface
+from PyPDF2 import PdfReader #To read PDF Files
 
 #Upload PDF files
 st.header("My First Chatbot")
 with st.sidebar:
     st.title("Your Documents")
     file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text+=page.extract_text()
+        st.write(text)
+
+#Break it into chunks
+
+
